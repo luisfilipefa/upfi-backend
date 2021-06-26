2ad88be9c86ec2a62023ec4d9aab2a8fd66a36ce
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-26 18:39:13 -0300

    create migration to create posts table

---
+++ src/config/database/migrations/1624741591391-CreatePostsTable.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePostsTable1624741591391 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "posts",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "image",
+            type: "varchar",
+          },
+          {
+            name: "author_id",
+            type: "uuid",
+          },
+          {
+            name: "rocketUpCount",
+            type: "integer",
+            default: 0,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_users_posts",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["author_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("posts");
+  }
+}
