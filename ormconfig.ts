d6093d51ff21a54bf3835269826cd2096293e8f2
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-06-29 17:29:14 -0300

    update ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,6 @@
 export default {
   "type": "postgres",
-  "host": process.env.DB_HOST || "localhost",
-  "port": 5432,
-  "username": process.env.DB_USER || "docker",
-  "password": process.env.DB_PASSWORD || "docker",
-  "database": process.env.DB_NAME || "docker",
-  "extra": { "ssl": true },
+  "url": process.env.DATABASE_URL || "postgres://docker:docker@localhost:5432/docker",
   "migrations": ["./src/config/database/migrations/*.ts"],
   "entities": ["./src/models/*.ts"],
   "cli": {
